6e4decbb55535aa69bb76ffc913005bafe0a7a53
Ilya Prokshin <prokshin99@yandex.ru>
2020-04-28 23:04:59 +0500

    Добавьте файлы проекта.

---
+++ mb-back/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using mb_back.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace mb_back.Controllers
+{
+
+    public class AuthController : ControllerBase
+    {
+        private List<User> users = new List<User>
+        {
+            new User("a@a.a", "12345"),
+            new User("b@b.b", "12345")
+        };
+
+        [HttpPost("api/auth")]
+        public IActionResult Token([FromBody] User user)
+        {
+            var identity = GetIdentity(user.Email, user.Password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            return Ok(response);
+        }
+        private ClaimsIdentity GetIdentity(string email, string password)
+        {
+            User user = users.FirstOrDefault(x => x.Email == email && x.Password == password);
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            Console.WriteLine("не найден юзер");
+            return null;
+        }
+    }
+}
---
+++ mb-back/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mb_back.Models
+{
+    public class User
+    {
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Img { get; set; }
+
+        public User() { }
+        public User(string email, string password)
+        {
+            this.Email = email;
+            this.Password = password;
+            this.Img = null;
+        }
+    }
+}
